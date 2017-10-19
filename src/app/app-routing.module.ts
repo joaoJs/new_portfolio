5d0d98a2da0d3c98e0154085accb9868fcd2712b
Joao Campos <j.campos4g@gmail.com>
2017-10-19 11:30:20 -0400

    connected routerLinks to components and grid in artist page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
+import { ArtistComponent } from './pages/artist/artist.component';
+import { PianistComponent} from './pages/pianist/pianist.component';
 
 const routes: Routes = [
-  {path: '', component: AboutComponent}
+  {path: '', component: AboutComponent},
+  {path: 'artist', component: ArtistComponent},
+  {path: 'pianist', component: PianistComponent}
 ];
 
 @NgModule({
