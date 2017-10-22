f148b0d71c940b2d87312afba14527832b5a198d
Joao Campos <j.campos4g@gmail.com>
2017-10-21 23:29:19 -0400

    added map to footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AgmCoreModule } from '@agm/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './pages/about/about.component';
@@ -16,7 +18,10 @@ import { PianistComponent } from './pages/pianist/pianist.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBZmlw9qWUNZvc1jbUEZ8HQXHGTHXe3Jm4'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
